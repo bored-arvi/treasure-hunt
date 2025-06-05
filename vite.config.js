ac9b20d054e71906e71062884d632a6d6c2c6837
arvi <aravinth3007k@gmail.com>
2025-06-05 11:18:14 +0530

    Updated to include firebase database creation

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/treasure-hunt/',  // Replace 'treasure-hunt' with your actual GitHub repo name
+  base: '/',  // Replace 'treasure-hunt' with your actual GitHub repo name
 });
